2c359fdb4319f76f56675666305c553ec7513e4b
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2020-08-21 16:39:26 -0400

    Adding training script for baseline

---
+++ scripts/train_baseline.py
@@ -0,0 +1,35 @@
+import h5py
+import pytorch_lightning as pl
+from torch.utils.data import DataLoader
+
+from dii.pipeline.datautils import CompositeH5Dataset
+from dii.pipeline.transforms import default_pipeline
+from dii.models.base import AutoEncoder, BaseEncoder, BaseDecoder
+
+
+N_WORKERS = 8
+BATCH_SIZE = 64
+
+
+autoencoder = AutoEncoder(BaseEncoder(), BaseDecoder())
+with h5py.File("../data/raw/ion_images.h5", "r") as h5_file:
+    train_indices = np.array(h5_file["train"])
+    test_indices = np.array(h5_file["test"])
+    dev_indices = np.array(h5_file["dev"])
+
+# Load up the datasets
+train_dataset = CompositeH5Dataset(
+    "../data/raw/ion_images.h5", "true", default_pipeline, indices=train_indices
+)
+test_dataset = CompositeH5Dataset(
+    "../data/raw/ion_images.h5", "true", default_pipeline, indices=test_indices
+)
+dev_dataset = CompositeH5Dataset(
+    "../data/raw/ion_images.h5", "true", default_pipeline, indices=dev_indices
+)
+
+train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, num_workers=N_WORKERS)
+test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, num_workers=N_WORKERS)
+dev_loader = DataLoader(dev_dataset, batch_size=BATCH_SIZE, num_workers=N_WORKERS)
+
+trainer = pl.
