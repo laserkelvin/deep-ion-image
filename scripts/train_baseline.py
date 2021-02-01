20ef98c1fb8ce28c38f34f82b61461bf2ca4cfb6
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2021-02-01 08:11:15 -0500

    Working wandb sweeping for baseline
    Needed to include argparse to make it happen :P

--- scripts/train_baseline.py
+++ scripts/train_baseline.py
@@ -3,16 +3,53 @@ import numpy as np
 import pytorch_lightning as pl
 import torch
 import wandb
+import argparse
 from torch.utils.data import DataLoader
 
 from dii.pipeline.datautils import CompositeH5Dataset
-from dii.pipeline.transforms import default_pipeline
-from dii.models.base import AutoEncoder, BaseEncoder, BaseDecoder
-from dii.utils import checkpoint_callback
+from dii.models.base import AutoEncoder, Encoder, Decoder
+from dii.utils import load_yaml
 
+config = load_yaml("baseline.yml")
 
-N_WORKERS = 8
-BATCH_SIZE = 24
+parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+parser.add_argument('--in_channels', type=int, default=1, metavar='N',
+                    help='Number of input channels (default: 1)')
+parser.add_argument('--out_channels', type=int, default=1, metavar='N',
+                    help='Number of otuput channels (default: 1)')
+parser.add_argument('--latent_dim', type=int, default=128, metavar='N',
+                    help='Dimensionality of the latent vector (default: 1)')
+parser.add_argument('--batch_size', type=int, default=64, metavar='N',
+                    help='input batch size for training (default: 64)')
+parser.add_argument('--n_workers', type=int, default=8, metavar='N',
+                    help='Number of CPUs for dataloading (default: 9)')
+parser.add_argument('--epochs', type=int, default=50, metavar='N',
+                    help='number of epochs to train (default: 14)')
+parser.add_argument('--lr', type=float, default=1e-3, metavar='LR',
+                    help='learning rate (default: 1.0)')
+parser.add_argument('--train_seed', type=int, default=42, metavar='S',
+                    help='random seed (default: 1)')
+parser.add_argument('--test_seed', type=int, default=1923, metavar='S',
+                    help='random seed (default: 1)')
+parser.add_argument('--grad_clip', type=float, default=0., metavar='GC',
+                    help='Gradient clipping value (default: 0)')
+parser.add_argument('--activation', type=str, default="relu", metavar='GC',
+                    help='Name of activation function (default: relu)')
+parser.add_argument('--half', type=bool, default=False, metavar='GC',
+                    help='Flag for using half precision (default: False)')
+parser.add_argument('--dropout', type=float, default=0., metavar='GC',
+                    help='Dropout probability (default: 0)')
+parser.add_argument('--weight_decay', type=float, default=0., metavar='GC',
+                    help='L2 regularization (default: 0)')
+args = parser.parse_args()
+
+if args.half:
+    PRECISION = 16
+else:
+    PRECISION = 32
+
+wandb.init(config=config)
+wandb.config.update(args)
 
 if torch.cuda.is_available():
     GPU = 1
@@ -20,27 +57,38 @@ else:
     GPU = 0
 
 
-autoencoder = AutoEncoder(BaseEncoder(), BaseDecoder())
+model = AutoEncoder(**vars(args))
 
 with h5py.File("../data/raw/ion_images.h5", "r") as h5_file:
     train_indices = np.array(h5_file["train"])
     test_indices = np.array(h5_file["test"])
-    dev_indices = np.array(h5_file["dev"])
 
-# Load up the datasets
+# Load up the datasets; random seed is set for the training set
 train_dataset = CompositeH5Dataset(
-    "../data/raw/ion_images.h5", "true", default_pipeline, indices=train_indices
+    "../data/raw/128_ion_images.h5", "projection", indices=train_indices, seed=args.train_seed
 )
-#test_dataset = CompositeH5Dataset(
-#    "../data/raw/ion_images.h5", "true", default_pipeline, indices=test_indices
-#)
+test_dataset = CompositeH5Dataset(
+    "../data/raw/128_ion_images.h5", "projection", indices=test_indices, seed=args.test_seed
+)
 
-train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, num_workers=N_WORKERS)
-#test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, num_workers=N_WORKERS)
+train_loader = DataLoader(
+    train_dataset, batch_size=args.batch_size, num_workers=args.n_workers, pin_memory=False
+)
+test_loader = DataLoader(
+    test_dataset, batch_size=args.batch_size, num_workers=args.n_workers, pin_memory=False
+)
 
 logger = pl.loggers.WandbLogger(name="baseline", project="deep-ion-image")
-logger.watch(autoencoder, log="all")
+logger.watch(model, log="all")
+logger.log_hyperparams(config)
 
-trainer = pl.Trainer(logger=logger, max_epochs=30, gpus=GPU, accumulate_grad_batches=3)
+trainer = pl.Trainer(
+    max_epochs=args.epochs,
+    gpus=GPU,
+    accumulate_grad_batches=config["accumulate_grad_batches"],
+    gradient_clip_val=args.grad_clip,
+    precision=PRECISION,
+    logger=logger,
+)
 
-trainer.fit(autoencoder, train_loader)
+trainer.fit(model, train_loader, test_loader)
